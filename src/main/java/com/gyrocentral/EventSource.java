684e1fb608b653cc3c0ff5cd3a966fe14ff3ea0e
ttjb2 <tom.bauer95@gmail.com>
2019-07-18 11:44:41 -0500

    commit

--- src/main/java/com/gyrocentral/EventSource.java
+++ src/main/java/com/gyrocentral/EventSource.java
@@ -1,43 +1,43 @@
-package com.gyrocentral;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.time.LocalTime;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-@Controller
-public class EventSource {
-    private static final Logger log = LoggerFactory.getLogger(EventSource.class);
-
+//package com.gyrocentral;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.MediaType;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+//
+//import java.time.LocalTime;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//
+//@Controller
+//public class EventSource {
+//    private static final Logger log = LoggerFactory.getLogger(EventSource.class);
+//
 //    @Scheduled(cron = "*/2 * * * * *")
 //    public void checkScheduling(){
 //        log.info("functional");
 //
 //    }
-
-    @GetMapping(path="/EventSource")
-    public SseEmitter streamSseMvc() {
-        SseEmitter sseEmitter = new SseEmitter();
-        ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
-        sseMvcExecutor.execute(() -> {
-            try{
-                for (int i = 0; true; i++) {
-                    SseEmitter.SseEventBuilder event = SseEmitter.event().data("any string I want");
-                    sseEmitter.send(event);
-                    Thread.sleep(1000);
-                }
-            } catch(Exception ex) {
-                sseEmitter.completeWithError(ex);
-            }
-        });
-        return sseEmitter;
-    }
-
-}
+//
+//    @GetMapping(path="/EventSource")
+//    public SseEmitter streamSseMvc() {
+//        SseEmitter sseEmitter = new SseEmitter(Long.valueOf("5000"));
+//        ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
+//        sseMvcExecutor.execute(() -> {
+//                try{
+//                        SseEmitter.SseEventBuilder event = SseEmitter.event().data("~5sec have passed");
+//                        sseEmitter.send(event);
+//                        Thread.sleep(1000);
+//                } catch(Exception ex) {
+//                    sseEmitter.completeWithError(ex);
+//                }
+//
+//
+//        });
+//        return sseEmitter;
+//    }
+//
+//}
